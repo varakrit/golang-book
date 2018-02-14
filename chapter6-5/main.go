16babfd9acb9a2a8ff2c761d7377cb8b014fb4dc
Varakrit <varakrit@hotmail.com>
2018-02-14 10:21:49 +0700

    Add Chapter6-5 Function FizzBuzz

---
+++ chapter6-5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for number := 1; number <= 100; number++ {
+		fmt.Println(number, fizzBuzz(number))
+	}
+}
+
+func fizzBuzz(number int) string {
+	if number%15 == 0 {
+		return "FizzBuzz"
+	} else if number%3 == 0 {
+		return "Fizz"
+	} else if number%5 == 0 {
+		return "Buzz"
+	} else {
+		return ""
+	}
+}
