89db2c25a89bb0aa9e6f8454f5c06d77e53b760f
Varakrit <varakrit@hotmail.com>
2018-02-16 10:14:11 +0700

    Add Exercise Console-Weather

---
+++ exercise/console-weather.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(weatherCelsius(25, "Bangkok few cloud"))
+	fmt.Println(weatherCelsius(34, "Tak sunny"))
+	fmt.Println(weatherCelsius(17, "Phuket rainy"))
+	fmt.Println(weatherCelsius(9, "Chiang-mai cold"))
+}
+
+func weatherCelsius(celsius int, msg string) string {
+
+	display := ""
+
+	if celsius == 25 {
+		display = display + " _  _\n"
+		display = display + " _||_\n"
+		display = display + "|_  _|"
+	}
+
+	if celsius == 34 {
+		display = display + " _\n"
+		display = display + " _||_|\n"
+		display = display + " _|  |"
+	}
+
+	if celsius == 17 {
+		display = display + "    _\n"
+		display = display + "  |  |\n"
+		display = display + "  |  |"
+	}
+
+	if celsius == 9 {
+		display = display + " _\n"
+		display = display + "|_|\n"
+		display = display + " _|"
+	}
+
+	display = display + " c\n"
+	display = display + msg
+
+	return display
+}
