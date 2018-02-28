6dca8f6bf9751f5f8839b73f8dba28f98c35270d
Varakrit <varakrit@hotmail.com>
2018-02-28 10:35:11 +0700

    Update package

--- exercise/the-return-of-vending-machine.go
+++ exercise/the-return-of-vending-machine.go
@@ -1,70 +1,15 @@
 package main
 
 import "fmt"
+import "github.com/varakrit/vendingMachine"
 
-type VendingMachine struct {
-	insertedMoney int
-	coins map[string] int
-	items map[string] int
-}
-
-func (m VendingMachine) InsertedMoney() int {
-	return m.insertedMoney
-}
-
-func (m *VendingMachine) InsertCoin(coin string) {
-	m.insertedMoney += m.coins[coin]
-}
-
-func (m *VendingMachine) SelectSD() string {
-	price := m.items["SD"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "SD" + m.change(change)
-}
-
-func (m *VendingMachine) SelectCC() string {
-	price := m.items["CC"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "CC" + m.change(change)
-
-}
-
-func (m *VendingMachine) SelectDW() string {
-	price := m.items["DW"]
-	change := m.insertedMoney - price
-	m.insertedMoney = 0
-	return "DW" + m.change(change)
-}
-
-
-func (m VendingMachine) change(c int) string {
-	var str string
-	values := [...]int{10, 5, 2, 1}
-	coins := [...]string{"T","F","TW","O"}
-
-	for i := 0; i < len(values); i++ {
-		if c>= values[i] {
-			str += ", " + coins[i]
-			c -= values[i]
-			i--
-		}
-	}
-
-	return str
-}
-
-func (vm *VendingMachine) CoinReturn() string {
-	coins := vm.change(vm.insertedMoney)
-	vm.insertedMoney = 0
-	return coins[2:len(coins)]
-}
+var coins = map[string]int{"T": 10, "F": 5, "TW": 2, "O": 1}
+var items = map[string]int{"SD": 18, "CC": 12, "DW": 7}
 
 func main() {
-	var coins = map[string] int{"T": 10, "F": 5, "TW": 2, "O": 1}
-	var items = map[string] int{"SD": 18, "CC": 12, "DW": 7}
-	vm := VendingMachine{coins: coins, items: items}
+
+	// vm := vendingMacine.VendingMachine{coins: coins, items: items}
+	vm := vendingMachine.NewVendingMachine(coins, items)
 	fmt.Println("Inserted Money:", vm.InsertedMoney())
 	// Inserted Money: 0
 	vm.InsertCoin("T")
